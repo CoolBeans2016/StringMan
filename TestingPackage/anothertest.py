df4c6a043c30a59b7c5574fdf3134e389d9d91d2
God Of War <koolbeansmarketing@gmail.com>
2016-04-28 14:14:24 -0400

    This is a test

---
+++ TestingPackage/anothertest.py
@@ -0,0 +1,10 @@
+#from TestingPackage.mystring import MyString
+from myString import MyString
+s=MyString("Hello")
+
+print(s.getVowels())
+print(s.getSubstring())
+print(s.getCharList())
+print(s.indexOf())
+print(s.removeChar())
+print(s.invert())
